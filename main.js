5b8bad12979cbc9cffdf746688c511fcf26d2bc7
新垣陸 <shingakiriku@aragakirikunoMacBook-Air.local>
2019-01-07 11:42:53 +0900

    second main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
 const input = prompt('今日の天気は？次の3つのうち、いずれかの文字列を入力してくささい。(晴れ or くもり or 雨)');
 
 // ここより下に条件分岐のコードを記述してください。
-if(input == '晴れ') {
+if(input === '晴れ') {
     window.alert('今日は気持ちの良い一日になりそうですね！');
-} else if(input == 'くもり') {
+} else if(input === 'くもり') {
     window.alert('どんよりした天気ですが今日も一日がんばりましょう！');
-} else if(input == '雨') {
+} else if(input === '雨') {
     window.alert('家でゆっくり過ごすのも良いと思いますよ！');
 } else {
     window.alert('晴れ・くもり・雨以外の文字列には対応していません');
